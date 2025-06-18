5b2f818de56858543871a25e7c93c66b4c96138b
H4K0R <victorcarmo2003@gmail.com>
2025-06-18 02:39:01 -0300

    First commit!

---
+++ Packages/goodsignal.lua
@@ -0,0 +1,4 @@
+local REQUIRED_MODULE = require(script.Parent._Index["stravant_goodsignal@0.3.0"]["goodsignal"])
+export type Connection = REQUIRED_MODULE.Connection 
+export type Signal<T...> = REQUIRED_MODULE.Signal<T...>
+return REQUIRED_MODULE
