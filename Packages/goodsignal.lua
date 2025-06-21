3b2b6765b45e089c34eb04a9e214d05ef760039e
victorcarmo2003 <victorcarmo2003@gmail.com>
2025-06-20 21:08:12 -0300

    First commit

--- Packages/goodsignal.lua
+++ Packages/goodsignal.lua
@@ -1,4 +1,1 @@
-local REQUIRED_MODULE = require(script.Parent._Index["stravant_goodsignal@0.3.0"]["goodsignal"])
-export type Connection = REQUIRED_MODULE.Connection 
-export type Signal<T...> = REQUIRED_MODULE.Signal<T...>
-return REQUIRED_MODULE
+return require(script.Parent._Index["l3g5nd_goodsignal@2.0.0"]["goodsignal"])
--- ServerPackages/trove.lua
+++
@@ -1,1 +0,0 @@
-return require(script.Parent._Index["sleitnick_trove@1.5.0"]["trove"])
